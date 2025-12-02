35d73e6e694041127a237d79fb07e93cae3602f2
rityjacob <rityjacob@gmail.com>
2025-12-02 12:57:30 -0500

    added mail, need to fix errors

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const path = require('path');
-const { title } = require('process');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
+const logger = require('./Logger/logger.js')
 
 
 const PORT = process.env.PORT || 5001
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//logger
+app.use(logger);
+
+
 
 async function sendEmail(to_email,subject,body) {
 
@@ -18,16 +25,86 @@ async function sendEmail(to_email,subject,body) {
             user : process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
         },
-    })
+    });
+
+    return transporter.sendMail({
+        from: process.env.SMTP_USER,
+        to: to_email,
+        subject,
+        text: body,
+    });
     
 }
 
-app.get('/api/contact',(req,res) =>{
-    
-    res.json({ success: true, msg:'Message Sent'})
+app.get('/api/contact', (req,res)=>{
+    res.json('hello');
+})
+
+
+app.post('/api/contact',async(req,res) =>{
+
+    res.json('hey');
+    const {name, subject, message} = req.body;
+
+    if(!name||!subject||!message){
+     return res.status(400).json({succes: false, msg:'All fields are required'})}
+
+    try {
+        // Building email
+        const emailSubject = `New message from ${name}: ${subject}`;
+        const body = ` From : ${name}
+        Message: ${message}
+        `;
+
+        //Send email, calling fn above
+        await sendEmail(process.env.SMTP_USER,subject, body);
+
+        //Respond to frontend
+        res.json({sucess: true, msg:'Message sent'});
+    }catch(err){
+        console.log(`Error occured `,err);
+        res.status(500).json({ success: false, msg: 'Email Failed' });
+    }
+
+
+   
 
 });
 
 app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
 
-console.log('Hello');
+// console.log('Hello');
+
+// response mail, add later 
+
+ // if(!name||!subject||!message){
+    //     return res.status(400).json({msg:'All fields are required'})
+    // }
+
+    // const adminHtml = `‹div style="font-family: Arial,sans-serif; padding: 20px; color: #333;">
+    //                     <h2>New Contact Form Submission</h2>
+    //                     ‹p><strong>Name:</strong>
+    //                     ${name} </ p>
+    //                     <p><strong>subject</ strong>
+    //                     ${subject} </ p>
+    //                     <p><strong>Message:</strong></p>
+    //                     <p>${message}</p>‹/ div>`;
+
+    // const userHtml = `‹div style="font-family: Arial, sans-serif; padding: 20px; color: #333;"> 
+    // <h2>New Contact Form Submission</h2> <p>Dear ${name}</p>
+    // <p>Thank you for contacting us</p>
+    // ‹/ div>`;
+
+    // const adminMailOptions = {
+    //     from: process.env.SMTP_USER,
+    //     to: process.env.SMTP_USER,
+    //     subject: `${subject}`,
+    //     html: adminHtml,
+    // };
+
+    // const userMailOption = {
+    //     from: process.env.SMTP_USER,
+    //     to: process.env.SMTP_USER,
+    //     subject: `Thank you for contacting`,
+    //     html: userHtml,
+    // };
